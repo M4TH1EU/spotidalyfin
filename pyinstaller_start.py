9d3cba14f5e4b1b255ab7588dedd4fdcd4de1ad7
Mathieu Broillet <mathieu@broillet.ch>
2024-09-04 22:22:06 +0200

    fix pyinstaller ssl errors

--- pyinstaller_start.py
+++ pyinstaller_start.py
@@ -1,4 +1,10 @@
+import os
+import sys
+
 from spotidalyfin import cli
+
+# Fix SSL cert issue with PyInstaller
+os.environ['SSL_CERT_FILE'] = os.path.join(sys._MEIPASS, 'certifi', 'cacert.pem')
 
 if __name__ == '__main__':
     cli.app()
