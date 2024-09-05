a1fd38312eb334fea174eb526464b106f7c69e9b
Mathieu Broillet <mathieu@broillet.ch>
2024-09-05 18:54:44 +0200

    add github build action

--- build.sh
+++ build.sh
@@ -6,24 +6,24 @@ wait 2
 rm -rf buildenv build dist *.spec
 python -m venv buildenv
 source buildenv/bin/activate
-#pip install -r requirements.txt
-#pip install pyinstaller
+pip install -r requirements.txt
+pip install pyinstaller
 
 # cleaner (less junk)
-CFLAGS="-g0 -Wl,--strip-all" \
-        pip install \
-        --no-cache-dir \
-        --compile \
-        --global-option=build_ext \
-        --global-option="-j 4" \
-        -r requirements.txt
-CFLAGS="-g0 -Wl,--strip-all" \
-        pip install \
-        --no-cache-dir \
-        --compile \
-        --global-option=build_ext \
-        --global-option="-j 4" \
-        pyinstaller
+#CFLAGS="-g0 -Wl,--strip-all" \
+#        pip install \
+#        --no-cache-dir \
+#        --compile \
+#        --global-option=build_ext \
+#        --global-option="-j 4" \
+#        -r requirements.txt
+#CFLAGS="-g0 -Wl,--strip-all" \
+#        pip install \
+#        --no-cache-dir \
+#        --compile \
+#        --global-option=build_ext \
+#        --global-option="-j 4" \
+#        pyinstaller
 
 pyinstaller --noconfirm --onefile --hidden-import spotidalyfin --add-binary spotidalyfin/spotidalyfin.secrets:./ --console --name spotidalyfin-${VERSION}_linux_x86_64 "pyinstaller_start.py"
 deactivate
