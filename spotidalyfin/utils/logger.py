ac28b8ed460d7c4cdf2f41918d388d1354e8ba43
Mathieu Broillet <mathieu@broillet.ch>
2025-07-19 19:31:40 +0200

    remove rich logger

--- spotidalyfin/utils/logger.py
+++ spotidalyfin/utils/logger.py
@@ -1,22 +1,12 @@
 import logging
 
-from rich.logging import RichHandler
-
 log = logging.getLogger("spotidalyfin")
 
 
 def setup_logger(debug: bool = False):
-    log.handlers.clear()
 
     # Prevent tidalapi from logging
     logging.getLogger('tidalapi.settings').disabled = True
     logging.getLogger('tidalapi.session').disabled = True
 
-    # Prevent rich from propagating logs to root logger
-    log.propagate = False
-
-    handler_with_formatter = RichHandler()
-    handler_with_formatter.setFormatter(logging.Formatter('%(message)s'))
-
-    log.addHandler(handler_with_formatter)
     log.setLevel(logging.DEBUG if debug else logging.INFO)
